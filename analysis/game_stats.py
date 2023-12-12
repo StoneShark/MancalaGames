a1ef862271f215700cdbdd14f643da835f288d60
Ann <ann.davies@comcast.net>
2023-12-12 05:26:14 -0500

    Another cut at game data and stats. A checkpoint while debugging.

--- analysis/game_stats.py
+++ analysis/game_stats.py
@@ -1,198 +1,110 @@
 # -*- coding: utf-8 -*-
-"""Goal:  compute stats on game lengths
-
-Created on Sun Jul 23 11:29:10 2023
+"""
+Created on Sat Dec  9 23:10:58 2023
 @author: Ann"""
 
-import argparse
-import dataclasses as dc
-import datetime
-import os
-import random
-import sys
 
+# %% imports
+
+import numpy as np
 import pandas as pd
-import tqdm
-
-from context import ai_player
-from context import game_log
-from context import man_config
-
-from game_interface import WinCond
+import scipy
 
 
-game_log.game_log.active = False
+# %% load data
+
+data = pd.read_csv('data/gstats_44_1000_1211_0933.csv',
+                   dtype={'name': str,
+                          'starter': bool,
+                          'turns': int,
+                          'outcome': str,
+                          'winner': bool,
+                          'passes': int,
+                          'repeats': int,
+                          'rounds': int,
+                          'rnd_turns': int},
+                   header=0,
+                   index_col='name')
 
 
-# %%  game index list
-
-ALL = 'all'
-
-PATH = '../GameProps/'
-BAD_CFG = 'all_params.txt'
-
-INDEX = [fname[:-4] for fname in os.listdir(PATH) if fname != BAD_CFG]
+# %%  fairness
 
 
-# %%  command line args
+WIN_SCORE = 4
+TIE_SCORE = 2
+EXPECTED_VAL = WIN_SCORE * 0.5
 
-def define_parser():
-    """Define the command line arguements."""
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--game', action='append',
-                        choices=list(INDEX) + [ALL],
-                        help="""Select the games to simulate. Use multiple
-                        options to select multiple games.""")
-
-    parser.add_argument('--nbr_runs', action='store',
-                        default=100, type=int,
-                        help="""Select the number of games to simulate.
-                        Default: %(default)s""")
-
-    parser.add_argument('--ai_player', action='store_true',
-                        help="""Use the minimaxer ai_player.
-                        Default: %(default)s""")
-
-    parser.add_argument('--output', action='store',
-                        help="""Output file. Default: %(default)s""")
-
-    return parser
+CONFIDENCE = 0.95
+CRIT_VALUE = scipy.stats.norm.ppf(1 - (1 - CONFIDENCE) / 2)
 
 
-def process_command_line():
+def win_cnt_agg(outcomes):
+    return sum(1 for val in outcomes if val == 'WIN')
 
-    global cargs
+cltd = pd.pivot_table(data[data.rnd_turns == 0],
+                      index='name',
+                      columns=['starter', 'winner'],
+                      values='outcome',
+                      aggfunc=win_cnt_agg)
 
-    parser = define_parser()
-    try:
-        cargs = parser.parse_args()
-    except argparse.ArgumentError:
-        parser.print_help()
-        sys.exit()
+cltd.columns = [f's{str(col[0])[0]}_win{str(col[1])[0]}'
+                for col in cltd.columns.values]
+cltd = cltd.fillna(0)
 
-    if not cargs.game:
-        cargs.game = INDEX
+def tie_cnt_agg(outcomes):
+    return sum(1 for val in outcomes if val == 'TIE')
 
-    if not cargs.output:
-        game_cnt = len(cargs.game)
-        gname = cargs.game[0] if game_cnt == 1 else str(game_cnt)
-        cargs.output = f'gstats_{gname}_{cargs.nbr_runs}'
-        # cargs.output += '_' + datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+ties = pd.pivot_table(data[data.rnd_turns == 0],
+                      index='name',
+                      columns=['starter'],
+                      values='outcome',
+                      aggfunc=tie_cnt_agg)
 
-    print(cargs)
+cltd['sT_tie'] = ties[True]
+cltd['sF_tie'] = ties[False]
 
-
-# %% data record and frame
-
-@dc.dataclass
-class GameRecord:
-
-    name: str
-    starter: bool
-    turns: int
-    outcome: WinCond
-    winner: object      # True, False or None
-
-    passes: int = dc.field(default=0, kw_only=True)
-    repeats: int = dc.field(default=0, kw_only=True)
-    rounds: int = dc.field(default=0, kw_only=True)
-    rnd_turns: int = dc.field(default=0, kw_only=True)
+cltd['nbr_runs'] = [sum(cltd.loc[game]) for game in cltd.index]
 
 
-def build_data_frame():
-    """Build a data frame with all the desired columns,
-    Include key game data (optional), raw game result tallys, two sum values
-    for win percents, and fairness columns."""
+# Determine if each game is symetrically fair,
+#   H0: prob(True win) == 0.5
+#   e.g. player=True wins as often as player=False
+#   False (not fair) likely means that there is a game implementation error
 
-    global data
-    data = pd.DataFrame(columns=[f.name for f in dc.fields(GameRecord)])
+cltd['score'] = (cltd['sT_winT'] + cltd['sF_winT']) * WIN_SCORE \
+              + (cltd['sT_tie'] + cltd['sF_tie']) * TIE_SCORE
+cltd['score_2'] = (cltd['sT_winT'] + cltd['sF_winT']) * (WIN_SCORE ** 2) \
+              + (cltd['sT_tie'] + cltd['sF_tie']) * (TIE_SCORE ** 2)
 
+cltd['t_win_pct'] = cltd['score'] / (WIN_SCORE * cltd['nbr_runs'])
 
-# %%  play and collect
+cltd['mean'] = cltd['score'] / cltd['nbr_runs']
+cltd['stddev'] = \
+    np.sqrt((cltd['score_2'] \
+             - ((cltd['score'] * cltd['score']) / cltd['nbr_runs']))
+            / (cltd['nbr_runs'] - 1))
 
-def test_one_game(game, pdict):
-    """Play one game, tally the counts in gstats"""
-
-    round_count = round_start = repeats = passes = 0
-
-    if cargs.ai_player:
-        tplayer = ai_player.AiPlayer(game, pdict)
-        fplayer = ai_player.AiPlayer(game, pdict)
-
-    for turns in range(10000 if game.info.rounds else 1000):
-
-        if not cargs.ai_player:
-            moves = game.get_moves()
-            assert moves, "Game didn't end right."
-            move = random.choice(moves)
-        else:
-            if game.turn:
-                move = tplayer.pick_move()
-            else:
-                move = fplayer.pick_move()
-
-        cond = game.move(move)
-
-        if cond == WinCond.REPEAT_TURN:
-            repeats += 1
-        if cond in (WinCond.WIN, WinCond.TIE):
-            break
-        if cond in (WinCond.ROUND_WIN, WinCond.ROUND_TIE):
-            round_count += 1
-            data.loc[len(data)] = dc.asdict(GameRecord(
-                game.info.name, game.starter, turns, cond.name, game.turn,
-                passes=passes,
-                repeats=repeats,
-                rounds=round_count,
-                rnd_turns=turns - round_start if round_count else 0))
-            round_start = turns
-            repeats = passes = 0
-
-            if game.new_game(cond, new_round_ok=True):
-                break
-
-        if game.info.mustpass:
-            if game.test_pass():
-                passes += 1
-
-    end_cond = cond.name if cond else None
-    data.loc[len(data)] = dc.asdict(GameRecord(
-        game.info.name, game.starter, turns, end_cond, game.turn,
-        passes=passes,
-        repeats=repeats,
-        rounds=round_count,
-        rnd_turns=turns - round_start if round_count else 0))
+cltd['fair'] = (((cltd['mean'] - EXPECTED_VAL)
+                  / (cltd['stddev'] / np.sqrt(cltd['nbr_runs'])))
+                 < CRIT_VALUE)
 
 
-def play_one_config(gname):
-    """For one game configuration, play cargs.nbr_runs number of games.
-    Collect data in the associated GameStats."""
+# Determine if each game is fair in terms of the starter,
+#  starter is equally likely win versus non-starter
 
-    for cnt in tqdm.tqdm(range(cargs.nbr_runs)):
+cltd['st_score'] = (cltd['sT_winT'] + cltd['sF_winF']) * WIN_SCORE \
+                + (cltd['sT_tie'] + cltd['sF_tie']) * TIE_SCORE
+cltd['st_score_2'] = (cltd['sT_winT'] + cltd['sF_winF']) * (WIN_SCORE ** 2) \
+                + (cltd['sT_tie'] + cltd['sF_tie']) * (TIE_SCORE ** 2)
 
-        game, pdict = man_config.make_game(PATH + gname + '.txt')
-        if cnt < cargs.nbr_runs // 2:
-            game.starter = game.turn = True
-        else:
-            game.starter = game.turn = False
+cltd['st_win_pct'] = cltd['st_score'] / (WIN_SCORE * cltd['nbr_runs'])
 
-        test_one_game(game, pdict)
+cltd['st_mean'] = cltd['st_score'] / cltd['nbr_runs']
+cltd['st_stddev'] = \
+    np.sqrt((cltd['st_score_2'] \
+             - ((cltd['st_score'] * cltd['st_score']) / cltd['nbr_runs']))
+            / (cltd['nbr_runs'] - 1))
 
-
-def play_them_all():
-
-    for gname in cargs.game:
-        print(gname)
-        play_one_config(gname)
-
-    data.to_csv(f'data/{cargs.output}.csv')
-
-
-# %%
-
-process_command_line()
-build_data_frame()
-
-play_them_all()
+cltd['st_fair'] = (((cltd['mean'] - EXPECTED_VAL)
+                  / (cltd['stddev'] / np.sqrt(cltd['nbr_runs'])))
+                 < CRIT_VALUE)
