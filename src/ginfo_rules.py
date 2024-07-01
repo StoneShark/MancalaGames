e8794a57382fdf2991638dfed069aaf13cb85195
Ann <ann.davies@comcast.net>
2024-07-01 09:25:20 -0400

    corrections to capt_conflict rule

--- src/ginfo_rules.py
+++ src/ginfo_rules.py
@@ -538,12 +538,15 @@ def build_rules():
     man_rules.add_rule(
         'capt_conflict',
         rule=lambda ginfo: any(
-            [ginfo.capt_min > ginfo.capt_max,
-             ginfo.evens and all(cval % 2 == 1 for cval in ginfo.capt_on),
-             ginfo.nosinglecapt and ginfo.capt_on == [1],
-             ginfo.capt_on and max(ginfo.capt_on) < ginfo.capt_min,
-             ginfo.capt_on and min(ginfo.capt_on) > ginfo.capt_max,
-             ]),
+            [(ginfo.capt_min and ginfo.capt_max
+                  and ginfo.capt_min > ginfo.capt_max),
+             (ginfo.capt_on and ginfo.evens
+                  and all(cval % 2 == 1 for cval in ginfo.capt_on)),
+             (ginfo.capt_min and ginfo.capt_on
+                  and max(ginfo.capt_on) < ginfo.capt_min),
+             (ginfo.capt_max and ginfo.capt_on
+                  and min(ginfo.capt_on) > ginfo.capt_max),
+              ]),
         msg='Selected capture mechanisms conflict (no captures)',
         warn=True)
         # this doesn't catch all conflicts :(
