02b08ee98d9b84cda463fea111bf0bf0c0818a84
Ann <ann.davies@comcast.net>
2024-07-16 05:07:29 -0400

    Removed inhibitor from the not_yet tests (it's now tested).

--- test/test_not_yet.py
+++ test/test_not_yet.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """The point of this file to get all of the source files
-included in the coverage report (make unit_tests).
+included in the coverage report (make all_tests).
 
 Created on Mon Jul 31 14:07:52 2023
 @author: Ann
@@ -16,7 +16,6 @@ from context import mancala_ui
 from context import play_mancala
 from context import play
 from context import montecarlo_ts
-from context import inhibitor
 
 
 def test_files_not():
