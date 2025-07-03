90cd43812a22ca595477f9b5c15584cdbe2ce27c
StoneShark <59318856+StoneShark@users.noreply.github.com>
2025-07-03 06:36:04 -0400

    Restructured find_similar as class so neighs doesn't need to be passed to every function.

--- analysis/find_similar.py
+++ analysis/find_similar.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Apr  3 10:45:03 2025
-
-@author: Ann
-"""
+@author: Ann"""
 
 # %% imports
 
@@ -26,8 +24,6 @@ PATH = '../GameProps/'
 TXTPART = '.txt'
 EXFILE = '_all_params.txt'
 
-ALL_GAMES = None
-
 TAB_IDX = 0
 OPTION_IDX = 1
 SKIP_TAB = 'skip'
@@ -37,25 +33,7 @@ GCONSTS = ckey.GAME_CONSTANTS
 GINFO = ckey.GAME_INFO
 
 
-# %% collection routines
-
-def load_game_files():
-    """Get a list of the game files, read the game_dict,
-    create a dictionary."""
-
-    global ALL_GAMES
-
-    ALL_GAMES = {}
-    for file in os.listdir(PATH):
-
-        if file[-4:] != TXTPART or file == EXFILE:
-            continue
-
-        game_dict = man_config.read_game(PATH + file)
-
-        game_name = game_dict[ckey.GAME_INFO][ckey.NAME]
-        ALL_GAMES[game_name] = game_dict
-
+# %% unused routines
 
 def get_params():
     """Read the game parameters file and return a list params."""
@@ -70,169 +48,204 @@ def get_params():
                 and LBL_OPT not in rec[OPTION_IDX]]
 
 
-def list_diffs(gdict1, gdict2):
-    """Create a list of the game parameters that are different
-    between the two games."""
+# %% neighbors class
 
-    dlist = []
-
-    if gdict1[ckey.GAME_CLASS] != gdict2[ckey.GAME_CLASS]:
-        dlist += [ckey.GAME_CLASS]
-
-    for param in ckey.GCONST_PARAMS:
-        if gdict1[GCONSTS][param] != gdict1[GCONSTS][param]:
-            dlist += [param]
-
-    for param in ckey.GINFO_PARAMS:
-
-        default = gi.GameInfo.get_default(param)
-        val1 = gdict1[GINFO].get(param, default)
-        val2 = gdict2[GINFO].get(param, default)
-        if val1 != val2:
-            dlist += [param]
-
-    return dlist
+class Neighbors:
+    """A class to support comparing and contrasting game
+    parameters."""
 
 
-def count_diffs(gdict1, gdict2):
-    """Count the number of differences between the two game
-    configurations."""
+    def __init__(self):
 
-    pdiffs = 0
-    for param in ckey.GINFO_PARAMS:
+        self.all_games = self.load_game_files()
+        self.neighs = self.find_similar()
 
-        default = gi.GameInfo.get_default(param)
-        val1 = gdict1[GINFO].get(param, default)
-        val2 = gdict2[GINFO].get(param, default)
-        if val1 != val2:
-            pdiffs += 1
-
-    return sum([pdiffs,
-
-        1 if gdict1[ckey.GAME_CLASS] != gdict2[ckey.GAME_CLASS] else 0,
-
-        sum(1 for param in ckey.GCONST_PARAMS
-               if gdict1[GCONSTS][param] != gdict1[GCONSTS][param]),
-        ])
+        self.print_summary()
 
 
-def find_similar():
-    """Create a dictionary where the keys are the number of
-    differences and the values are a list of game pairs with
-    those differences."""
+    def load_game_files(self):
+        """Get a list of the game files, read the game_dict,
+        create a dictionary."""
 
-    gname_list = list(ALL_GAMES.keys())
-    diff_dict = collections.defaultdict(list)
+        all_games = {}
+        for file in os.listdir(PATH):
 
-    for gname1, gname2 in it.combinations(gname_list, 2):
+            if file[-4:] != TXTPART or file == EXFILE:
+                continue
 
-        diffs = count_diffs(ALL_GAMES[gname1], ALL_GAMES[gname2])
-        diff_dict[diffs] += [(gname1, gname2)]
+            game_dict = man_config.read_game(PATH + file)
 
-    return diff_dict
+            game_name = game_dict[ckey.GAME_INFO][ckey.NAME]
+            all_games[game_name] = game_dict
 
-
-# %% output routines
-
-def print_summary(ndict):
-    """Print a summary of the results."""
-
-    print("Diff  Count")
-    for nbr_diff, pairs in sorted(ndict.items(), key=lambda item: item[0]):
-        print(f"{nbr_diff:3}: {len(pairs):4}")
-
-    for d in range(1, 3):
-        print(f"Pairs differing by {d} param(s):")
-        for g1, g2 in neighs[d]:
-            print(f'   {g1:20}  {g2:20}')
-        print()
-
-def one_game(game, ndict):
-    """Print the distances of all games from the one provided."""
-
-    print(game)
-    for nbr_diff, pairs in sorted(ndict.items(), key=lambda item: item[0]):
-
-        title = f"  {nbr_diff}:"
-        for game1, game2 in pairs:
-
-            match1 = game == game1
-            match2 = game == game2
-            if title and (match1 or match2):
-                if title:
-                    print(title)
-                    title = None
-
-            if match1:
-                print(f"     {game2}")
-            if match2:
-                print(f"     {game1}")
+        return all_games
 
 
-def print_line(values):
-    """Print a list of values in columns."""
+    def find_similar(self):
+        """Create a dictionary where the keys are the number of
+        differences and the values are a list of game pairs with
+        those differences.  Sort it by number of differences."""
 
-    print("".join(f"{str(val):20}" for val in values))
+        gname_list = list(self.all_games.keys())
+        diff_dict = collections.defaultdict(list)
+
+        for gname1, gname2 in it.combinations(gname_list, 2):
+
+            diffs = self.count_diffs(self.all_games[gname1],
+                                     self.all_games[gname2])
+            diff_dict[diffs] += [(gname1, gname2)]
+
+        return sorted(diff_dict.items(), key=lambda item: item[0])
 
 
-def print_option_line(gname_list, option):
-    """Prepare and print one line for option for each game."""
+    @staticmethod
+    def list_diffs(gdict1, gdict2):
+        """Create a list of the game parameters that are different
+        between the two games."""
 
-    pvalues = [option]
+        dlist = []
 
-    for game in gname_list:
-        default = gi.GameInfo.get_default(option)
-        pvalues += [ALL_GAMES[game][GINFO].get(option, default)]
+        if gdict1[ckey.GAME_CLASS] != gdict2[ckey.GAME_CLASS]:
+            dlist += [ckey.GAME_CLASS]
 
-    print_line(pvalues)
+        for param in ckey.GCONST_PARAMS:
+            if gdict1[GCONSTS][param] != gdict1[GCONSTS][param]:
+                dlist += [param]
+
+        for param in ckey.GINFO_PARAMS:
+
+            default = gi.GameInfo.get_default(param)
+            val1 = gdict1[GINFO].get(param, default)
+            val2 = gdict2[GINFO].get(param, default)
+            if val1 != val2:
+                dlist += [param]
+
+        return dlist
 
 
-def print_diff_table(neighs, gname_list):
-    """Print a table of the values that differ."""
+    @staticmethod
+    def count_diffs(gdict1, gdict2):
+        """Count the number of differences between the two game
+        configurations.  Keys are not required in ginfo so need
+        to look up the default if it is not in the dictionary.
+        game class and both constansts are required."""
 
-    # gname_list = sorted(gname_list)
+        pdiffs = 0
+        for param in ckey.GINFO_PARAMS:
 
-    options = set()
-    for g1, g2 in it.combinations(gname_list, 2):
-        options |= set(list_diffs(ALL_GAMES[g1], ALL_GAMES[g2]))
+            default = gi.GameInfo.get_default(param)
+            val1 = gdict1[GINFO].get(param, default)
+            val2 = gdict2[GINFO].get(param, default)
+            if val1 != val2:
+                pdiffs += 1
 
-    print_line(["Option"] + gname_list)
+        return sum([pdiffs,
 
-    starters = [ckey.GAME_CLASS, ckey.HOLES, ckey.NBR_START]
-    for opt in starters:
-        if opt in options:
-            print_option_line(gname_list, opt)
+            1 if gdict1[ckey.GAME_CLASS] != gdict2[ckey.GAME_CLASS] else 0,
 
-    for opt in sorted(options):
-        if opt not in starters:
-            print_option_line(gname_list, opt)
+            sum(1 for param in ckey.GCONST_PARAMS
+                   if gdict1[GCONSTS][param] != gdict1[GCONSTS][param]),
+            ])
+
+
+    def print_summary(self):
+        """Print a summary of the results."""
+
+        print("Diff  Count")
+        for nbr_diff, pairs in self.neighs:
+
+            print(f"{nbr_diff:3}: {len(pairs):4}")
+
+        for d in range(1, 3):
+            print(f"Pairs differing by {d} param(s):")
+            for g1, g2 in self.neighs[d]:
+                print(f'   {g1:20}  {g2:20}')
+            print()
+
+
+    def one_game(self, game):
+        """Print the distances of all games from the one provided."""
+
+        print(game)
+        for nbr_diff, pairs in self.neighs:
+
+            title = f"  {nbr_diff}:"
+            for game1, game2 in pairs:
+
+                match1 = game == game1
+                match2 = game == game2
+                if title and (match1 or match2):
+                    if title:
+                        print(title)
+                        title = None
+
+                if match1:
+                    print(f"     {game2}")
+                if match2:
+                    print(f"     {game1}")
+
+
+    @staticmethod
+    def print_line(values):
+        """Print a list of values in columns."""
+
+        print("".join(f"{str(val):20}" for val in values))
+
+
+    def print_option_line(self, gname_list, option):
+        """Prepare and print one line for option for each game."""
+
+        pvalues = [option]
+
+        for game in gname_list:
+            default = gi.GameInfo.get_default(option)
+            pvalues += [self.all_games[game][GINFO].get(option, default)]
+
+        self.print_line(pvalues)
+
+
+    def print_diff_table(self, gname_list):
+        """Print a table of the values that differ."""
+
+        # gname_list = sorted(gname_list)
+
+        options = set()
+        for g1, g2 in it.combinations(gname_list, 2):
+            options |= set(self.list_diffs(self.all_games[g1],
+                                           self.all_games[g2]))
+
+        self.print_line(["Option"] + gname_list)
+
+        starters = [ckey.GAME_CLASS, ckey.HOLES, ckey.NBR_START]
+        for opt in starters:
+            if opt in options:
+                self.print_option_line(gname_list, opt)
+
+        for opt in sorted(options):
+            if opt not in starters:
+                self.print_option_line(gname_list, opt)
+
+
+    def uniquer(self, max_diffs):
+        """Return a list of games that do not have any other game within
+        max_diffs of it.
+
+        This is a bit flawed; for example, a unique game like Diffusion has
+        DiffusionV2 1 parameter away from it."""
+
+        games = set(self.all_games.keys())
+
+        for nbr_diff, pairs in self.neighs:
+
+            if nbr_diff > max_diffs:
+                break
+
+            for pair in pairs:
+                games -= set(pair)
+
+        return games
 
 
 # %%
 
-def uniquer(max_diffs, ndict):
-    """Return a list of games that do not have any other game within
-    max_diffs of it.
-
-    This is a bit flawed; for example, a unique game like Diffusion has
-    DiffusionV2 1 parameter away from it."""
-
-    games = set(ALL_GAMES.keys())
-
-    for nbr_diff, pairs in sorted(ndict.items(), key=lambda item: item[0]):
-
-        if nbr_diff > max_diffs:
-            break
-
-        for pair in pairs:
-            games -= set(pair)
-
-    return games
-
-
-
-# %%
-
-load_game_files()
-neighs = find_similar()
-print_summary(neighs)
+neighs = Neighbors()
