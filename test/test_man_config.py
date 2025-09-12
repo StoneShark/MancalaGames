fb514b029bfffc83fe7441ffe8fbd9734c5a9f03
StoneShark <59318856+StoneShark@users.noreply.github.com>
2025-09-12 12:45:20 -0400

    Move SimpleCapt to CrossCapt was confused about which chapter I was reading. Updated coverage for man_config.

--- test/test_man_config.py
+++ test/test_man_config.py
@@ -1102,7 +1102,7 @@ class TestConfig:
             man_config.DEFAULTS['inactive_color']
 
 
-    def test_game_overrides(self, mocker, tmp_path):
+    def test_game_overrides_1(self, mocker, tmp_path):
 
         path = os.path.join(tmp_path, 'mancala.ini')
         with open(path, 'w', encoding='utf-8') as file:
@@ -1121,6 +1121,55 @@ class TestConfig:
         assert config['grid_density'] == '25'
 
         config = man_config.ConfigData(None, 'my game')
+        assert config['button_size'] == '40'
+        assert config['grid_density'] == '25'
+
+
+    def test_game_overrides_2(self, mocker, tmp_path):
+        """Test with a variant qualified name that is present."""
+
+        path = os.path.join(tmp_path, 'mancala.ini')
+        with open(path, 'w', encoding='utf-8') as file:
+            print("""[default]
+                  button_size = 20
+                  grid_density = 25
+                  [my_game::var]
+                  button_size=40""", file=file)
+
+        # man_path finds the file where we just put it
+        mpath = mocker.patch.object(man_path, 'get_path')
+        mpath.return_value = path
+
+        config = man_config.ConfigData(None, 'diff game')
+        assert config['button_size'] == '20'
+        assert config['grid_density'] == '25'
+
+        config = man_config.ConfigData(None, 'my game::var')
+        assert config['button_size'] == '40'
+        assert config['grid_density'] == '25'
+
+
+    def test_game_overrides_3(self, mocker, tmp_path):
+        """Test with a variant qualified name that is not present
+        fall back to base game name."""
+
+        path = os.path.join(tmp_path, 'mancala.ini')
+        with open(path, 'w', encoding='utf-8') as file:
+            print("""[default]
+                  button_size = 20
+                  grid_density = 25
+                  [my_game]
+                  button_size=40""", file=file)
+
+        # man_path finds the file where we just put it
+        mpath = mocker.patch.object(man_path, 'get_path')
+        mpath.return_value = path
+
+        config = man_config.ConfigData(None, 'diff game')
+        assert config['button_size'] == '20'
+        assert config['grid_density'] == '25'
+
+        config = man_config.ConfigData(None, 'my game::var')
         assert config['button_size'] == '40'
         assert config['grid_density'] == '25'
 
